c837e10ce70b37a0d1cc32a5f3d0ca60dc92d5b9
Waimer28 <55116913+Waimer28@users.noreply.github.com>
2019-10-27 20:41:30 -0400

    CHalleNGe 2 platformer

---
+++ GonzalezR_Tutorial2/Assets/scripts/PlayerScript.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rd2d;
+
+    public float speed;
+
+    public Text score;
+
+    private int scoreValue = 0;
+
+    public Text winText;
+
+    public Text LivesText;
+
+    private int Lives;
+
+    public AudioClip musicClipOne;
+
+    public AudioClip musicClipTwo;
+
+    public AudioSource musicSource;
+
+    Animator anim;
+
+    private bool facingRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rd2d = GetComponent<Rigidbody2D>();
+        score.text = scoreValue.ToString();
+        winText.text = "";
+        Lives = 3;
+        LivesText.text = "";
+        setLivesText();
+        anim = GetComponent<Animator>();
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        musicSource.loop = true;
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float hozMovement = Input.GetAxis("Horizontal");
+        float vertMovement = Input.GetAxis("Vertical");
+        rd2d.AddForce(new Vector2(hozMovement * speed, vertMovement * speed));
+        if (Input.GetKey("escape"))
+        {
+            Application.Quit();
+        }
+        if (facingRight == false && hozMovement > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && hozMovement < 0)
+        {
+            Flip();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Coin")
+        {
+            scoreValue += 1;
+            score.text = scoreValue.ToString();
+            Destroy(collision.collider.gameObject);
+            SetCountText();
+        }
+        else if (collision.collider.tag == "Enemy")
+        {
+            Destroy(collision.collider.gameObject);
+            Lives = Lives - 1;
+            setLivesText();
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                rd2d.AddForce(new Vector2(0, 3), ForceMode2D.Impulse);
+                anim.SetInteger("State", 2);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            anim.SetInteger("State", 0);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetInteger("State", 1);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            anim.SetInteger("State", 0);
+        }
+    }
+    void SetCountText()
+    {
+        if (scoreValue == 4)
+        {
+            transform.position = new Vector2(-6.5f, -41.06f);
+            Lives = 3;
+            LivesText.text = "";
+            setLivesText();
+        }
+        if (scoreValue == 8)
+        {
+            musicSource.Stop();
+            musicSource.loop = false;
+            musicSource.clip = musicClipTwo;
+            musicSource.Play();
+            winText.text = "You win! Made by Ricardo Gonzalez";
+        }
+    }
+    void setLivesText()
+    {
+        LivesText.text = "Lives: " + Lives.ToString();
+        if (Lives == 0)
+        {
+            winText.text = "You Lose! Try again";
+            Destroy(gameObject);
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector2 Scaler = transform.localScale;
+        Scaler.x = Scaler.x * -1;
+        transform.localScale = Scaler;
+    }
+}  
